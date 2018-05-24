9e19df9718097d8e0c1d6badf508e105e27f0f61
Omar Garcia <isc.omargarcia@gmail.com>
2018-05-24 17:22:23 -0500

    Added tests for feedbacks

--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -9,4 +9,9 @@ class EmployeeTest < ActiveSupport::TestCase
     create_list(:review, 3, reviewer: @employee)
     assert_equal @employee.reviews.count, 3
   end
+
+  test 'as a reviewee I can have many feedbacks' do
+    create_list(:review, 3, reviewee: @employee)
+    assert_equal @employee.feedbacks.count, 3
+  end
 end
