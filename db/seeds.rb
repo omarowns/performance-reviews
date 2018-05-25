7d74e9c3a0d5617e5c91b04ad609464cb8b3b25a
Omar Garcia <isc.omargarcia@gmail.com>
2018-05-25 03:04:40 -0500

    Enable cors and added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+jd = ::Employee.create(name: 'John Doe')
+ts = ::Employee.create(name: 'Tony Stark')
+cp = ::Employee.create(name: 'Chris Pratt')
+og = ::Employee.create(name: 'Omar Garcia')
+
+::Review.create(reviewer: jd, reviewee: ts)
+::Review.create(reviewer: jd, reviewee: cp)
+::Review.create(reviewer: jd, reviewee: og)
+::Review.create(reviewer: cp, reviewee: og)
+::Review.create(reviewer: og, reviewee: ts)
+::Review.create(reviewer: og, reviewee: jd)
