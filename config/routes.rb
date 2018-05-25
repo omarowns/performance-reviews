32a99b29d55af635681d7145c30d237a9d68d80f
Omar Garcia <isc.omargarcia@gmail.com>
2018-05-25 15:11:07 -0500

    Attempt to serve react app through rails

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
     resources :employees
     resources :reviews, except: [:destroy]
   end
+
+  get '*path', to: "react#index", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
